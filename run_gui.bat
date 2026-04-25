2a411f9ac75991969cd07166673111b9378e8d9b
MichaelQiu <16895711+cpufreestyle@user.noreply.gitee.com>
2026-04-25 07:06:25 +0000

    chore: remove hardcoded run script

--- run_gui.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set TCL_LIBRARY=C:\Program Files\Python311\tcl\tcl8.6
-set TK_LIBRARY=C:\Program Files\Python311\tcl\tk8.6
-"C:\Program Files\Python311\python.exe" "C:\Users\michael\.qclaw\workspace\network-reset-tool\network_reset_gui.py"
